6733d382380fc2201e2bcaba167e789bb458cf88
Gavin <yes@gmail.com>
2022-12-12 12:35:06 -0600

    final

---
+++ MainApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+app_name = 'pizzas'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('pizzas',views.pizzas,name='pizzas'),
+    path('MainApp/<int:pizza_id>',views.pizza,name='pizza'),
+    path('Comment/<int:pizza_id>/',views.Comment,name='Comment'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
